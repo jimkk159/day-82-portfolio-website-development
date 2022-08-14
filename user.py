ffc09cee2633a56bb613795c481b5b9d53098629
Jim <jimkk159@gmail.com>
2022-08-14 19:37:35 +0800

    encryption password by hash and salt

--- user.py
+++ user.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 
+# encryption
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # self import
 from extension import db
@@ -15,10 +17,19 @@ def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         query_user = User.query.filter_by(email=login_form.email.data).first()
+
+        # check user exist
         if not query_user:
             flash('User not exists')
             return redirect(url_for('user.login'))
-        return redirect(url_for('home'))
+
+        # check password
+        if check_password_hash(query_user.password, login_form.password.data):
+            return redirect(url_for('home'))
+        else:
+            flash('Password wrong')
+            return redirect(url_for('user.login'))
+
     return render_template('login.html', login_form=login_form)
 
 
@@ -28,17 +39,20 @@ def register():
     register_form = RegisterForm()
     if register_form.validate_on_submit():
 
+        # confirm password
         if register_form.password.data != register_form.password_confirm.data:
             flash('Confirm password fail')
             return redirect(url_for('user.register'))
 
+        # repeat register
         if User.query.filter_by(email=register_form.email.data).first():
             flash('Email already exists')
             return redirect(url_for('user.login'))
 
         new_user = User(email=register_form.email.data,
                         name=register_form.name.data,
-                        password=register_form.password.data)
+                        password=generate_password_hash(register_form.password.data, method='pbkdf2:sha256',
+                                                        salt_length=8))
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('home'))
