17d73bde5ab15e93b5b58f24886a85e511ffa2e7
Jim <jimkk159@gmail.com>
2022-08-14 18:01:24 +0800

    initial register and login redirect

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
 
 
 # self import
@@ -14,8 +14,11 @@ user_blueprint = Blueprint('user', __name__)
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        print(login_form.email.data, login_form.password.data)
-        new_user = User(email=login_form.email.data, name=login_form.name.data, password=login_form.password.data)
+        query_user = User.query.filter_by(email=login_form.email.data).first()
+        if not query_user:
+            flash('User not exists')
+            return redirect(url_for('user.login'))
+        return redirect(url_for('home'))
     return render_template('login.html', login_form=login_form)
 
 
@@ -24,12 +27,13 @@ def login():
 def register():
     register_form = RegisterForm()
     if register_form.validate_on_submit():
-        print(register_form.email.data, register_form.name.data, register_form.password.data,
-              register_form.password_confirm.data)
+        if User.query.filter_by(email=register_form.email.data).first():
+            flash('Email already exists')
+            return redirect(url_for('user.login'))
         new_user = User(email=register_form.email.data,
                         name=register_form.name.data,
                         password=register_form.password.data)
         db.session.add(new_user)
         db.session.commit()
-
+        return redirect(url_for('home'))
     return render_template('register.html', register_form=register_form)
