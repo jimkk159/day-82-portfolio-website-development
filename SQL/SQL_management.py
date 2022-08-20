e465c4a78131478a0c81ba277ed1b2be6a6d6525
Jim <jimkk159@gmail.com>
2022-08-21 01:56:07 +0800

    Debug HERKUO

--- SQL/SQL_management.py
+++ SQL/SQL_management.py
@@ -22,7 +22,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password = db.Column(db.String(), nullable=False)
 
     # Post
     posts = relationship("Post", back_populates="author")
